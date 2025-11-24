3946eeaa96e96510724e9a4508a29876793c025f
Martin Ribotta <martinribotta.dev@outlook.com.ar>
2025-11-24 11:29:31 -0300

    Refactor code structure for improved readability and maintainability

--- components/AboutMeParts/ImagePortrait.tsx
+++ components/AboutMeParts/ImagePortrait.tsx
@@ -4,21 +4,19 @@ export default function ImagePortrait() {
     <div className="flex justify-center">
       <div className="hidden md:block">
         <Image
-          src={"https://placehold.co/500"}
+          src={"/backgrounds/my-photo.png"}
           alt={"About Me Image"}
           width={500}
           height={500}
-          unoptimized
           className="rounded-lg"
         />
       </div>
       <div className="block md:hidden">
         <Image
-          src={"https://placehold.co/200"}
+          src={"/backgrounds/my-photo.png"}
           alt={"About Me Image"}
           width={200}
           height={200}
-          unoptimized
           className="rounded-lg"
         />
       </div>
